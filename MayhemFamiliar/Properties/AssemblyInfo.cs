a2d8fdcb22df21ed9b9d0d63b66701d9614b6c86
poslog <black_tp@hotmail.com>
2025-08-05 06:49:12 +0900

    パース対象メッセージにGREMessageType_QueuedGameStateMessageを追加
    対戦相手のアクションがちゃんと実況できるようになった

--- MayhemFamiliar/Properties/AssemblyInfo.cs
+++ MayhemFamiliar/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      ビルド番号
 //      リビジョン
 //
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 [assembly: NeutralResourcesLanguage("ja-JP")]
