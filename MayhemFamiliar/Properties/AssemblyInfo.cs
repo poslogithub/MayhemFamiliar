60b7ff4ecb4d2e361bcab7cc4d1fbcb603c4632b
poslog <black_tp@hotmail.com>
2025-08-14 08:12:43 +0900

    実況の主体が異なるバグを修正

--- MayhemFamiliar/Properties/AssemblyInfo.cs
+++ MayhemFamiliar/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //      ビルド番号
 //      リビジョン
 //
-[assembly: AssemblyVersion("0.1.3")]
-[assembly: AssemblyFileVersion("0.1.3")]
+[assembly: AssemblyVersion("0.1.4")]
+[assembly: AssemblyFileVersion("0.1.4")]
 [assembly: NeutralResourcesLanguage("ja")]
